d146746ce25831fa3f3b8543942f32d3dd321473
Jamal <faizanjamal2018@u.northwestern.edu>
2019-01-18 22:08:49 -0600

    9.156 added click listener to render the selected post in other section.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
 
     componentDidMount() {
@@ -27,12 +28,17 @@ class Blog extends Component {
             })
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
     render () {
-        const posts = this.state.posts.map( posts => {
+        const posts = this.state.posts.map( post => {
                 return <Post 
-                    title={posts.title}
-                    key={posts.id}
-                    author={posts.author} />
+                    title={post.title}
+                    key={post.id}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
             }
         )
 
@@ -42,7 +48,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
