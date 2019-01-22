fe3b2b4219981ce84c21755cba578687d5a607e2
Jamal <faizanjamal2018@u.northwestern.edu>
2019-01-21 18:17:58 -0600

    11.187 added link component from react-router-dom so app doesn't reload each time a link is clicked. embedded route handling within the link component in nav bar

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
 // import axios from 'axios';
 // import axios from '../../axios';
+// import Post from '../../components/Post/Post';
 
-import Post from '../../components/Post/Post';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -13,12 +16,19 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/* <Route path="/" exact render={ () => <h1>Home</h1>}/>
+                <Route path="/"  render={ () => <h1>Home 2</h1>}/> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
